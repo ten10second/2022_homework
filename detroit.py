9a31589ac980a5472c78f54a5683fd11860c5fe7
Zhimiao Shi <91169754+ten10second@users.noreply.github.com>
2022-03-31 22:54:22 +0800

    初始化 dataframe

--- detroit.py
+++ detroit.py
@@ -1,8 +1,18 @@
+from tkinter.font import names
 import matplotlib.pyplot as plt 
 import os
 import pandas as pd 
 import numpy as np
 import csv
 
-file_path = 'D:\\codefield\\2022_homework\\trip_distribution.csv'
+file_path = 'D://codefield//2022_homework//trip_distribution.csv'
 
+colums = ['1', '2', '3', '4', 'total_origin', 'future_origin']
+index = ['1', '2', '3', '4', 'total_destination', 'future_destination']
+df = pd.read_csv(file_path,header = None,delimiter=',')
+data = pd.DataFrame(df)
+
+data.index = index
+data.columns = colums
+
+print(data)
