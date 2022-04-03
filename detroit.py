cf55a7ae90487f73c1e2ebba4b579d677457a848
Zhimiao Shi <91169754+ten10second@users.noreply.github.com>
2022-04-03 15:33:38 +0800

    epoch1 还没加迭代

--- detroit.py
+++ detroit.py
@@ -2,9 +2,8 @@
 # Author：21216492
 # Date：2022/04/02
 
-
-
-from tkinter.font import names
+from calendar import EPOCH
+from cmath import nan
 import matplotlib.pyplot as plt 
 import os
 import pandas as pd 
@@ -25,4 +24,42 @@ data.columns = colums
 data['Total_generate'] = data.apply(lambda x: x.sum(), axis=1)
 data.loc['Total_attract'] = data.apply(lambda x: x.sum(), axis=0)
 
+# estimated future origins and estimated future destinations
+
+Estimated_future_generation = [1200, 1050, 380, 770]
+future_generation_dict = dict(zip(colums, Estimated_future_generation))
+Estimated_future_attraction = [670, 730, 950, 1050]
+future_attraction_dict = dict(zip(colums, Estimated_future_attraction))
+
+SUM = sum(Estimated_future_attraction)
+
+data['generation_growth'] = ''
+data.loc['attract_growth'] = ''
 print(data)
+
+# calculate the growth of the generate and attraction
+
+for i in range(len(colums)):
+    data.iat[i, 5] = Estimated_future_generation[i] / data.iat[i, 4]
+    data.iat[5, i] = Estimated_future_attraction[i] / data.iat[4, i]
+
+G = data.at['Total_attract', 'Total_generate'] / SUM
+
+epoch = 1
+
+for i in range(len(colums)):
+    for j in range(len(colums)):
+
+        data.iat[i, j] = data.iat[i, j] * G * data.iat[i, 5] * data.iat[5, j]
+
+# update the total_attract and total_generation
+data['Total_generate'] = data.iloc[0:4, :].sum(axis=0)
+data.loc['Total_attract'] = data.iloc[:, 0:5].sum(axis=0)  
+
+for i in range(len(colums)):
+    data.iat[i, 5] = Estimated_future_generation[i] / data.iat[i, 4]
+    data.iat[5, i] = Estimated_future_attraction[i] / data.iat[4, i]
+
+
+print(data)
+data.to_csv('epoch1.csv', sep='\t')
