fe5e46b5c0db76bd7dd2fee39cfca3b1e77a3eb2
Zhimiao Shi <91169754+ten10second@users.noreply.github.com>
2022-04-03 10:35:17 +0800

    simply the input data

--- detroit.py
+++ detroit.py
@@ -13,12 +13,16 @@ import csv
 
 file_path = 'D://codefield//2022_homework//trip_distribution.csv'
 
-colums = ['1', '2', '3', '4', 'total_origin', 'future_origin']
-index = ['1', '2', '3', '4', 'total_destination', 'future_destination']
-df = pd.read_csv(file_path,header = None,delimiter=',')
-data = pd.DataFrame(df)
+colums = ['1', '2', '3', '4']
+index = ['1', '2', '3', '4']
 
+df = pd.read_csv(file_path,header = None,delimiter=',')
+
+data = pd.DataFrame(df)
 data.index = index
 data.columns = colums
+
+data['Total_generate'] = data.apply(lambda x: x.sum(), axis=1)
+data.loc['Total_attract'] = data.apply(lambda x: x.sum(), axis=0)
 
 print(data)
