343f1a9a7fe5b1c2a08830bbd71e43f05818bf88
Zhimiao Shi <91169754+ten10second@users.noreply.github.com>
2022-04-09 11:07:19 +0800

    finished: maintain the three dataframes

---
+++ Attraction trip-end balancing.py
@@ -0,0 +1,75 @@
+
+import os
+from turtle import distance
+import pandas as pd 
+import numpy as np
+import csv
+import argparse
+
+
+
+# calculate the f_cij
+def resistent(distance):
+    return format(float(100)/float(distance**2), '.2f')
+
+
+distance_matrix = np.array([
+    [10, 15, 20],
+    [15, 8, 17],
+    [20, 17, 8]
+], dtype=float)
+
+zones = 3
+
+resistent_matrix = np.zeros([zones, zones])
+
+for i in range(distance_matrix.shape[0]):
+    for j in range(distance_matrix.shape[1]):
+        resistent_matrix[i][j] = resistent(distance_matrix[i][j])
+
+# print(resistent_matrix)
+
+file_path = 'demo2.csv'
+
+colums = ['1', '2', '3']
+index = ['1', '2', '3']
+
+df = pd.read_csv(file_path,header = None,delimiter=',')
+
+curr_data = pd.DataFrame(df)
+curr_data.index = index
+curr_data.columns = colums
+
+curr_data['O_i'] = curr_data.apply(lambda x: x.sum(), axis=1)
+curr_data.loc['D_j'] = curr_data.apply(lambda x: x.sum(), axis=0)
+
+# print(curr_data)  # 当前OD矩阵
+
+# the second matrix: B * D * resistence
+B = [1.0, 1.0, 1.0]
+BD_matrix = np.zeros([3, 3])
+for i in range(zones):
+    BD_matrix[i] = np.multiply(resistent_matrix[i], curr_data.loc['D_j'][:3])
+
+BD_dataframe = pd.DataFrame(BD_matrix, index=index, columns=colums)
+BD_dataframe['bd_sum'] = BD_dataframe.apply(lambda x: x.sum(), axis=1)
+print(BD_dataframe)
+
+
+T_matrix = np.zeros([zones, zones])
+T_dataframe = pd.DataFrame(T_matrix, index=index, columns=colums)
+
+for i in index:
+    for j in colums:
+        T_dataframe.at[i,j] = curr_data.at[i, 'O_i'] * (BD_dataframe.at[i, j]/ BD_dataframe.at[i, 'bd_sum'])
+T_dataframe.loc['t_sum'] = T_dataframe.apply(lambda x: x.sum(), axis=0)
+
+print(T_dataframe)
+for zone in colums:
+    i = colums.index(zone)
+    if T_dataframe.at['t_sum', zone] == 0 and curr_data.at['D_j', zone] == 0:
+        B[i] = 1.0
+    else:
+        B[i] = curr_data.at['D_j', zone] / T_dataframe.at['t_sum', zone]
+
+print(B)
