53d1a7993a925624ba934e6811db03176e34cd12
Zhimiao Shi <91169754+ten10second@users.noreply.github.com>
2022-05-02 01:22:30 +0800

    计算信号在各基站的停留时间

--- week 11/poi.py
+++ week 11/poi.py
@@ -16,9 +16,28 @@ gps_data.columns = colums
 person_1 = gps_data.query('isdn == 5264')
 person_2 = gps_data.query('isdn == 8586')
 person_3 = gps_data.query('isdn == 5264')
-person_1.to_csv('week 11\person_1', sep=',', index=False, header=colums)
-person_2.to_csv('week 11\person_2', sep=',', index=False, header=colums)
-person_3.to_csv('week 11\person_3', sep=',', index=False, header=colums)
+# person_1.to_csv('week 11\person_1', sep=',', index=False, header=colums)
+# person_2.to_csv('week 11\person_2', sep=',', index=False, header=colums)
+# person_3.to_csv('week 11\person_3', sep=',', index=False, header=colums)
+
+did_list = []
+did_during = []
+did_pos = []
+cur_did = person_1.at[0, 'did']
+inital_time = datetime.strptime(person_1.at[0, 'timestamp'], '%Y/%m/%d %H:%M')
+
+for i in range(1, len(person_1)):
+    if person_1.at[i, 'did'] == cur_did:
+        now_time = datetime.strptime(person_1.at[i, 'timestamp'], '%Y/%m/%d %H:%M')
+        during_time = now_time - inital_time
+    else: 
+        did_list.append(cur_did)  
+        did_pos.append((person_1.at[i, 'lng'], person_1.at[i, 'lat']))  
+        did_during.append(during_time)
+        cur_did = person_1.at[i, 'did']
+        inital_time = datetime.strptime(person_1.at[i, 'timestamp'], '%Y/%m/%d %H:%M') 
+
+print(did_during)
 # a = gps_data.at[3, 'timestamp']
 # b = gps_data.at[1, 'date']
 # print(a)
